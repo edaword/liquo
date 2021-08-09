933fc4d665830485d87edb6bbc4d67d7a730ae62
edaword <edawordwang@gmail.com>
2021-08-08 23:21:50 -0400

    added flutter charts dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'package:charts_flutter/flutter.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
